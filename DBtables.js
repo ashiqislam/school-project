0f08bc0328616267fa1f37417a41ca5afb18666a
Wes-11 <36212819+Wes-11@users.noreply.github.com>
2018-11-15 01:50:53 -0500

    Create DBtables.js

---
+++ DBtables.js
@@ -0,0 +1,32 @@
+var mysql = require('mysql');
+
+var db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "mydb"
+
+});
+db.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "CREATE TABLE Users (userID INT AUTO_INCREMENT PRIMARY KEY, userName VARCHAR(30), pass VARCHAR(30))";
+  db.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("user table created");
+  });
+  var sql = "CREATE TABLE beers (beerID INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30), type VARCHAR(30), rating int, price int, description VARCHAR(255), pictureFilePath VARCHAR(255))";
+  //picture probably doesn't need to have length 255 but this accomodates for bad filepathing at least
+
+  db.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("beers table created");
+  });
+  var sql = "CREATE TABLE favorites (name CHAR(30) PRIMARY KEY, rating int, beerID int, userID int, FOREIGN KEY (beerID) REFERENCES beers (beerID), FOREIGN KEY (userID) REFERENCES Users (userID))";
+  //really this is just a review history but we can sort by rating in descending order, getting unique users is easy because we will store user ID#s
+  //also lets us make use of AVG for average score
+  db.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("favorites table created");
+  });
+});
